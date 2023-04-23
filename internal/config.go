356af727a8648bcff6309650b78f3f43af0c89c6
Paul Buetow <paul@buetow.org>
2023-04-23 13:02:37 +0300

    remove comments for the obvious

--- internal/config.go
+++ internal/config.go
@@ -21,20 +21,17 @@ type config struct {
 func newConfig(configFile string) (config, error) {
 	var config config
 
-	// Open the file
 	file, err := os.Open(configFile)
 	if err != nil {
 		return config, err
 	}
 	defer file.Close()
 
-	// Read the file content
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		return config, err
 	}
 
-	// Parse the JSON content
 	err = json.Unmarshal(bytes, &config)
 	if err != nil {
 		return config, err
