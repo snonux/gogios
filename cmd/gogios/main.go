3abf436db869c99586ff55af9bd562a33d114607
Paul Buetow <paul@buetow.org>
2023-04-24 09:10:03 +0300

    add version string

--- cmd/gogios/main.go
+++ cmd/gogios/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"time"
 
 	"codeberg.org/snonux/gogios/internal"
 )
 
+const versionStr = "v0.0.0"
+
 func main() {
 	configFile := flag.String("cfg", "/etc/gogios.json", "The config file")
 	timeout := flag.Int("timeout", 5, "Global timeout in minutes")
 	renotify := flag.Bool("renotify", false, "Renotify all unhandled")
+	version := flag.Bool("version", false, "Display version")
 	flag.Parse()
+
+	if *version {
+		fmt.Printf("This is Gogios version %s; (C) by Paul Buetow\n", versionStr)
+		fmt.Println("https://codeberg.org/snonux/gogios")
+		return
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(),
 		time.Duration(*timeout)*time.Minute)
