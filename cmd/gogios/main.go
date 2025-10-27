ba653a0e4b33c645f78face477b09e4f2880d2df
Paul Buetow <paul@buetow.org>
2025-10-27 23:39:25 +0200

    bunp version

--- cmd/gogios/main.go
+++ cmd/gogios/main.go
@@ -9,7 +9,7 @@ import (
 	"codeberg.org/snonux/gogios/internal"
 )
 
-const versionStr = "v1.2.0-develop"
+const versionStr = "v1.2.0"
 
 func main() {
 	configFile := flag.String("cfg", "/etc/gogios.json", "The config file")
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module codeberg.org/snonux/gogios
 
 go 1.24
+
+require github.com/magefile/mage v1.15.0 // indirect
